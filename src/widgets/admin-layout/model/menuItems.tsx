d853898af438a200e2cee415cbefe81863e24c1e
rudney5000 <rudneygueko@gmail.com>
2026-03-09 16:47:35 +0300

    fix: menu remove others items

--- src/widgets/admin-layout/model/menuItems.tsx
+++ src/widgets/admin-layout/model/menuItems.tsx
@@ -31,14 +31,4 @@ export const menuItems: MenuItem[] = [
   item(<Link to="/admin/category">Category</Link>, "category", <AppstoreOutlined />),
   item(<Link to="/admin/currency">Currency</Link>, "Currency", <DollarOutlined />),
   item(<Link to="/admin/organizer">Organizer</Link>, "Organizer", <Users size={18}/>),
-  item("Users", "sub1", <UserOutlined />, [
-    item("Tom", "3"),
-    item("Bill", "4"),
-    item("Alex", "5"),
-  ]),
-  item("Teams", "sub2", <TeamOutlined />, [
-    item("Team 1", "6"),
-    item("Team 2", "8"),
-  ]),
-  item("Files", "9", <FileOutlined />),
 ];
