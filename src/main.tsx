c9d76dcbdbc660adf686005e76229e7cc51c8f93
rudney5000 <rudneyngueko@gmail.com>
2026-03-16 00:43:55 +0300

    feat: initialisation of i18n

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router'
 import { router } from './app/router/router.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
+import "./shared/config/i18n"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
