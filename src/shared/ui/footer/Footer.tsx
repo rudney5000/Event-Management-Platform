57ee5f1c5a8c7c1f674bcaa9c2e4c40c1ce7d3dd
rudney5000 <rudneyngueko@gmail.com>
2026-03-15 23:05:44 +0300

    feat: add footer

--- src/shared/ui/footer/Footer.tsx
+++ src/shared/ui/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { ChefHat } from 'lucide-react'
 
 export default function Footer() {
     return (
-        <footer className="bg-indigo-500  text-secondary-content mt-auto">
+        <footer className="bg-indigo-500 rounded-xl text-secondary-content mt-auto">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
                 <div className="flex flex-col md:flex-row items-center justify-between gap-4">
                     <div className="flex items-center gap-2">
