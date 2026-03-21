db1b520dec9cfd176dbaadb990a65f9089329c9d
rudney5000 <rudneyngueko@gmail.com>
2026-03-22 00:23:03 +0300

    feat(auth): add Zod schemas for login and login DTO

--- src/features/auth/model/schema/loginSchema.ts
+++ src/features/auth/model/schema/loginSchema.ts
@@ -1,8 +1,27 @@
 import { z } from "zod"
 
+export const loginSchemaDto = z.object({
+    id: z.string(),
+    email: z.string().email({ message: "please enter a valid number"}),
+    password: z.string().min(6)
+})
+
 export const loginSchema = z.object({
     email: z.string().email({ message: "please enter a valid number"}),
     password: z.string().min(6)
 })
 
+export const loginResponseSchema = z.object({
+    accessToken: z.string(),
+    refreshToken: z.string(),
+    user: z.object({
+        id: z.string(),
+        email: z.string().email(),
+        name: z.string().optional(),
+        roles: z.array(z.string()).optional(),
+    }),
+});
+
+export type LoginResponse = z.infer<typeof loginResponseSchema>;
 export type LoginFormData = z.infer<typeof loginSchema>
+export type LoginDtoFormData = z.infer<typeof loginSchemaDto>
